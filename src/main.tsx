dce354f72708fea72086879f09dc68f55cf995a4
Matthew Pagan <mastapegs01@gmail.com>
2023-06-18 14:49:16 -0400

    added router

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,19 @@ import ReactDOM from "react-dom/client";
 import { App } from "./app";
 import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
