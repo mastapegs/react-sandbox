c16703b78149e8ee44e68a803336ca21fdb1e551
Matthew Pagan <mastapegs01@gmail.com>
2023-06-18 15:22:39 -0400

    routing

--- src/router/Root.tsx
+++ src/router/Root.tsx
@@ -1,9 +1,17 @@
-import { Counter } from "../feature/counter/Counter";
+import { Outlet, Link } from "react-router-dom";
 
 export const Root = () => {
   return (
     <>
-      <Counter />
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/counter">Counter</Link>
+        </li>
+      </ul>
+      <Outlet />
     </>
   );
 };
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Root } from "./Root";
 import { ErrorPage } from "./ErrorPage";
+import { Counter } from "../feature/counter/Counter";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <h1>Home</h1> },
+      {
+        path: "counter",
+        element: <Counter />,
+      },
+    ],
   },
 ]);
