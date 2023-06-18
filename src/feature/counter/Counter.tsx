3536dcb9b55496093f83cae062901101478847e0
Matthew Pagan <mastapegs01@gmail.com>
2023-06-18 16:59:40 -0400

    add flex and refactor button data

--- src/feature/counter/Counter.tsx
+++ src/feature/counter/Counter.tsx
@@ -7,6 +7,7 @@ import {
   selectCount,
   selectIncrementOn,
 } from "./counterSlice";
+import classes from "./Counter.module.css";
 
 export const Counter = () => {
   const dispatch = useAppDispatch();
@@ -14,22 +15,34 @@ export const Counter = () => {
   const incrementOn = useAppSelector(selectIncrementOn);
 
   const amountToIncrement = 33;
+  const buttonsData = [
+    { text: "Increment", onClick: () => dispatch(increment()) },
+    { text: "Decrement", onClick: () => dispatch(decrement()) },
+    {
+      text: `Increment by ${amountToIncrement}`,
+      onClick: () => dispatch(incrementByAmount(amountToIncrement)),
+    },
+    {
+      text: "Increment every second",
+      onClick: () => dispatch(incrementEverySecond()),
+      disabled: incrementOn,
+    },
+  ];
+  const buttons = buttonsData.map((data) => (
+    <button
+      id={data.text}
+      onClick={data.onClick}
+      disabled={data.disabled !== undefined ? data.disabled : false}
+    >
+      {data.text}
+    </button>
+  ));
 
   return (
     <>
       <h1>Count</h1>
       <h2>{count}</h2>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
-      <button onClick={() => dispatch(incrementByAmount(amountToIncrement))}>
-        Increment by {amountToIncrement}
-      </button>
-      <button
-        disabled={incrementOn}
-        onClick={() => dispatch(incrementEverySecond())}
-      >
-        Increment every second
-      </button>
+      <div className={classes.buttonSetContainer}>{buttons}</div>
     </>
   );
 };
