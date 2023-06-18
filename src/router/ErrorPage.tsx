9ef07c7919701cd956697c32d071e2fd588f8106
Matthew Pagan <mastapegs01@gmail.com>
2023-06-18 15:11:59 -0400

    refactor error page

--- src/router/ErrorPage.tsx
+++ src/router/ErrorPage.tsx
@@ -1,1 +1,30 @@
-export const ErrorPage = () => <h1>404</h1>;
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+const ErrorWarning = () => (
+  <>
+    <h1>Oops!</h1>
+    <p>Sorry, an unexpected error has occurred.</p>
+  </>
+);
+
+export const ErrorPage = () => {
+  const error = useRouteError();
+  console.error(error);
+
+  if (isRouteErrorResponse(error))
+    return (
+      <div id="error-page">
+        <ErrorWarning />
+        <p>
+          {error.status} {error.statusText}
+        </p>
+      </div>
+    );
+
+  return (
+    <div id="error-page">
+      <ErrorWarning />
+      <p>Unknown Error</p>
+    </div>
+  );
+};
